ff69bfadd6c7e347dd3ed387a4c5f71878e4be3a
hamza <hamza472khan@gmail.com>
2025-12-19 08:58:58 +0500

    nodejs

---
+++ JavaScript/Class_4/Athmetic operators/Arthmatic operator/script.js
@@ -0,0 +1,17 @@
+//assignment operator
+// x++;
+// x=x+1;
+// x+=1;
+
+// let a = 502;
+// let b = 30;
+// let c = 23;
+
+// d = a+b;
+// console.log(d);
+
+
+// let x = 10;
+// let y = 20;
+
+// console.log(x+=y*2);
