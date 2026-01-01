87e1ffe8549ea6b3b48a2a61cffe2e7ffa99e72b
hamza <hamza472khan@gmail.com>
2026-01-01 11:06:48 +0500

    added number methods

---
+++ JavaScript/class_13/numberMethod/numberMethod.js
@@ -0,0 +1,28 @@
+//isNoN()
+//!isNoN()
+
+let x = 23;
+console.log(isNaN(x)); // false
+console.log(!isNaN(x)); // true
+
+//infinite
+
+let x2 = 3/0;
+let str = "finite";
+console.log(isFinite(x));
+console.log(str);
+
+let b = 10;
+let integer = "integer";
+let zero = 0;
+let float = 4.8;
+let negative = -10;
+
+console.log(Number.isInteger(b));
+console.log(Number.isInteger(integer));
+console.log(Number.isInteger(zero));
+console.log(Number.isInteger(float));
+console.log(Number.isInteger(nagetive));
+
+
+
