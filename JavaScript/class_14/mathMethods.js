f125fc9682f23737e656613b7c539ca38627b1e8
hamza <hamza472khan@gmail.com>
2026-01-02 09:16:47 +0500

    added js methods

---
+++ JavaScript/class_14/mathMethods.js
@@ -0,0 +1,16 @@
+//Math Methods
+
+//max()  : maximum
+//min()  : minimum
+//pow()  : Power
+//sqrt() : Square Root
+
+let numbers1 = Math.max(2, 6, 9, 33, 644, 0);
+let numbers2 = Math.min(2, 6, 9, 90, 34, 234);
+console.log(numbers1);
+console.log(numbers2);
+
+console.log(Math.pow(5, 2)); //first baswe and second is power 5*5= 25
+
+console.log(Math.sqrt(64));
+
