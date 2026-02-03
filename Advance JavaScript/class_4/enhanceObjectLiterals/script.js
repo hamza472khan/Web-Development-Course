d9661c299d3027d66d649ccfd30fb576f92ea545
hamza <hamza472khan@gmail.com>
2026-02-03 11:01:52 +0500

    EnhanceObjectLiterals | spread operator

---
+++ Advance JavaScript/class_4/enhanceObjectLiterals/script.js
@@ -0,0 +1,18 @@
+// enhance object literals
+
+// function displayUserDetails (name,age,gender){
+//     // let name = name;
+//     // let age = age;  
+//     // let gender = gender;
+
+//     const userDetails = {
+//         name,
+//         age,
+//         gender,
+//         displayDetails(){
+//             console.log(`Name: ${this.name}, Age: ${this.age}, Gender: ${this.gender}`);
+//         }   
+//     };
+//     userDetails.displayDetails();
+
+// }
